faba5de9b4b623f88a263359f99d8a9a59268d44
Tom Raviv <tomr@wix.com>
2021-06-14 11:34:22 +0300

    start transition to mocha

--- packages/toky/test/core.spec.ts
+++ packages/toky/test/core.spec.ts
@@ -43,78 +43,117 @@ const ignoreSpaceTokenizer = <T extends string>(input: T) =>
     },
   });
 
-test("1", defaultTokenizer, [{ value: "1", type: "text", start: 0, end: 1 }]);
+describe("1", () => {
+  it("1", () => {
+    test("1", defaultTokenizer, [
+      { value: "1", type: "text", start: 0, end: 1 },
+    ]);
+  });
 
-test("1'23'", defaultTokenizer, [
-  { value: "1", type: "text", start: 0, end: 1 },
-  { value: "'23'", type: "string", start: 1, end: 5 },
-]);
+  it("1'23'", () => {
+    test("1'23'", defaultTokenizer, [
+      { value: "1", type: "text", start: 0, end: 1 },
+      { value: "'23'", type: "string", start: 1, end: 5 },
+    ]);
+  });
 
-test("1`23`", defaultTokenizer, [
-  { value: "1", type: "text", start: 0, end: 1 },
-  { value: "`23`", type: "string", start: 1, end: 5 },
-]);
+  it("1`23`", () => {
+    test("1`23`", defaultTokenizer, [
+      { value: "1", type: "text", start: 0, end: 1 },
+      { value: "`23`", type: "string", start: 1, end: 5 },
+    ]);
+  });
 
-test(`1"23"`, defaultTokenizer, [
-  { value: "1", type: "text", start: 0, end: 1 },
-  { value: '"23"', type: "string", start: 1, end: 5 },
-]);
+  it(`1"23"`, () => {
+    test(`1"23"`, defaultTokenizer, [
+      { value: "1", type: "text", start: 0, end: 1 },
+      { value: '"23"', type: "string", start: 1, end: 5 },
+    ]);
+  });
 
-test(`1/*"23"*/`, defaultTokenizer, [
-  { value: "1", type: "text", start: 0, end: 1 },
-  { value: '/*"23"*/', type: "multi-comment", start: 1, end: 9 },
-]);
+  it(`1/*"23"*/`, () => {
+    test(`1/*"23"*/`, defaultTokenizer, [
+      { value: "1", type: "text", start: 0, end: 1 },
+      { value: '/*"23"*/', type: "multi-comment", start: 1, end: 9 },
+    ]);
+  });
 
-test(`1/*"23"`, defaultTokenizer, [
-  { value: "1", type: "text", start: 0, end: 1 },
-  { value: '/*"23"', type: "unclosed-comment", start: 1, end: 7 },
-]);
+  it(`1/*"23"`, () => {
+    test(`1/*"23"`, defaultTokenizer, [
+      { value: "1", type: "text", start: 0, end: 1 },
+      { value: '/*"23"', type: "unclosed-comment", start: 1, end: 7 },
+    ]);
+  });
 
-test(`1//"23"`, defaultTokenizer, [
-  { value: "1", type: "text", start: 0, end: 1 },
-  { value: '//"23"', type: "line-comment", start: 1, end: 7 },
-]);
+  it(`1//"23"`, () => {
+    test(`1//"23"`, defaultTokenizer, [
+      { value: "1", type: "text", start: 0, end: 1 },
+      { value: '//"23"', type: "line-comment", start: 1, end: 7 },
+    ]);
+  });
 
-test(`1//"23"\n4`, defaultTokenizer, [
-  { value: "1", type: "text", start: 0, end: 1 },
-  { value: '//"23"\n', type: "line-comment", start: 1, end: 8 },
-  { value: "4", type: "text", start: 8, end: 9 },
-]);
+  it(`1//"23"\n4`, () => {
+    test(`1//"23"\n4`, defaultTokenizer, [
+      { value: "1", type: "text", start: 0, end: 1 },
+      { value: '//"23"\n', type: "line-comment", start: 1, end: 8 },
+      { value: "4", type: "text", start: 8, end: 9 },
+    ]);
+  });
 
-test(`(1)`, defaultTokenizer, [
-  { value: "(", type: "(", start: 0, end: 1 },
-  { value: "1", type: "text", start: 1, end: 2 },
-  { value: ")", type: ")", start: 2, end: 3 },
-]);
+  it(`(1)`, () => {
+    test(`(1)`, defaultTokenizer, [
+      { value: "(", type: "(", start: 0, end: 1 },
+      { value: "1", type: "text", start: 1, end: 2 },
+      { value: ")", type: ")", start: 2, end: 3 },
+    ]);
+  });
 
-test(`{1}`, defaultTokenizer, [
-  { value: "{", type: "{", start: 0, end: 1 },
-  { value: "1", type: "text", start: 1, end: 2 },
-  { value: "}", type: "}", start: 2, end: 3 },
-]);
+  it(`{1}`, () => {
+    test(`{1}`, defaultTokenizer, [
+      { value: "{", type: "{", start: 0, end: 1 },
+      { value: "1", type: "text", start: 1, end: 2 },
+      { value: "}", type: "}", start: 2, end: 3 },
+    ]);
+  });
 
-test(`[1]`, defaultTokenizer, [
-  { value: "[", type: "[", start: 0, end: 1 },
-  { value: "1", type: "text", start: 1, end: 2 },
-  { value: "]", type: "]", start: 2, end: 3 },
-]);
+  it(`[1]`, () => {
+    test(`[1]`, defaultTokenizer, [
+      { value: "[", type: "[", start: 0, end: 1 },
+      { value: "1", type: "text", start: 1, end: 2 },
+      { value: "]", type: "]", start: 2, end: 3 },
+    ]);
+  });
 
-test(` `, defaultTokenizer, [{ value: " ", type: "space", start: 0, end: 1 }]);
-test(`  `, defaultTokenizer, [
-  { value: "  ", type: "space", start: 0, end: 2 },
-]);
+  it(` `, () => {
+    test(` `, defaultTokenizer, [
+      { value: " ", type: "space", start: 0, end: 1 },
+    ]);
+  });
 
-test(`\t`, defaultTokenizer, [
-  { value: "\t", type: "space", start: 0, end: 1 },
-]);
+  it(`  `, () => {
+    test(`  `, defaultTokenizer, [
+      { value: "  ", type: "space", start: 0, end: 2 },
+    ]);
+  });
 
-test(`1 2`, defaultTokenizer, [
-  { value: "1", type: "text", start: 0, end: 1 },
-  { value: " ", type: "space", start: 1, end: 2 },
-  { value: "2", type: "text", start: 2, end: 3 },
-]);
+  it(`\t`, () => {
+    test(`\t`, defaultTokenizer, [
+      { value: "\t", type: "space", start: 0, end: 1 },
+    ]);
+  });
 
-test(`1 2`, ignoreSpaceTokenizer, [
-  { value: "1", type: "text", start: 0, end: 1 },
-  { value: "2", type: "text", start: 2, end: 3 },
-]);
+  it(`1 2`, () => {
+    test(`1 2`, defaultTokenizer, [
+      { value: "1", type: "text", start: 0, end: 1 },
+      { value: " ", type: "space", start: 1, end: 2 },
+      { value: "2", type: "text", start: 2, end: 3 },
+    ]);
+  });
+
+  it(`1 2`, () => {
+    test(`1 2`, ignoreSpaceTokenizer, [
+      { value: "1", type: "text", start: 0, end: 1 },
+      { value: "2", type: "text", start: 2, end: 3 },
+    ]);
+  });
+});
