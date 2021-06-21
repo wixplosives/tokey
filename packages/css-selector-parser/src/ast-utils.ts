43a98eeaf346bc18873b999cddc7fa51f2ef760f
Ido Rosenthal <idoros@gmail.com>
2021-06-21 14:00:02 +0300

    feat: selector walk provide index and nodes

--- packages/css-selector-parser/src/ast-utils.ts
+++ packages/css-selector-parser/src/ast-utils.ts
@@ -1,4 +1,4 @@
-import type { SelectorNode, SelectorList } from "./ast-types";
+import type { SelectorNode, SelectorList, Containers } from "./ast-types";
 
 export interface WalkOptions {
   visitList?: SelectorNode["type"][];
@@ -6,58 +6,112 @@ export interface WalkOptions {
 }
 const nestEnd = Symbol(`nest-end`);
 export function walk(
-  current: SelectorNode | SelectorList,
-  visit: (node: SelectorNode, parents: SelectorNode[]) => number | undefined,
+  topNode: SelectorNode | SelectorList,
+  visit: (
+    node: SelectorNode,
+    index: number,
+    nodes: SelectorNode[],
+    parents: SelectorNode[]
+  ) => number | undefined,
   options: WalkOptions = {}
 ): void {
-  const parents: SelectorNode[][] = [[]];
-  const toVisit: Array<SelectorNode | SelectorList | typeof nestEnd> = [
-    current,
-  ];
+  // set initial top nodes to traverse
+  const toVisit: Array<SelectorNode | typeof nestEnd> = Array.isArray(topNode)
+    ? [...topNode]
+    : [topNode];
+  // initiate context
+  const context = createWalkContext(topNode);
   let skipAmount = -1;
+  // iterate nodes
   while (toVisit.length) {
     const current = toVisit.shift()!;
-    const currentParents = parents[parents.length - 1];
-    // visit only allowed nodes (also skip selector list)
     if (current === nestEnd) {
-      parents.pop();
+      // end of nested level
+      context.up();
       continue;
     } else if (
-      !Array.isArray(current) &&
       (!options.ignoreList || !options.ignoreList.includes(current.type)) &&
       (!options.visitList || options.visitList.includes(current.type))
     ) {
-      skipAmount = visit(current, currentParents) ?? -1;
+      // visit node
+      skipAmount =
+        visit(
+          current,
+          context.indexInSelector,
+          context.nodesInSelector,
+          context.parents
+        ) ?? -1;
+      context.next();
     }
+    // setup before next iteration
     if (skipAmount === Infinity) {
       // stop all: fast bail out
       return;
     } else if (skipAmount >= 0) {
-      // skip next levels
+      // skip levels
       while (skipAmount > 0 && toVisit.length) {
         const next = toVisit.shift()!;
         if (next === nestEnd) {
           skipAmount--;
+          context.up();
         }
       }
-    } else {
-      // add nested
-      if (Array.isArray(current)) {
-        // add sub selectors
-        toVisit.push(...current);
-      } else if (isWithNodes(current)) {
-        // add nested nodes
-        parents.push([...currentParents, current]);
-        toVisit.unshift(...current.nodes, nestEnd);
-      }
+    } else if (isWithNodes(current)) {
+      // set context for nested nodes
+      context.insertNested(current);
+      // add nested to visit list
+      toVisit.unshift(...current.nodes, nestEnd);
     }
   }
+}
+
+interface WalkContext {
+  parents: SelectorNode[];
+  indexInSelector: number;
+  nodesInSelector: SelectorNode[];
+  up(): void;
+  next(): void;
+  insertNested(node: ContainerWithNodes): void;
+}
+function createWalkContext(topNode: SelectorNode | SelectorList) {
+  const prevIndex: number[] = [];
+  const prevParents: SelectorNode[][] = [[]];
+  const context: WalkContext = {
+    parents: [],
+    indexInSelector: 0,
+    nodesInSelector: Array.isArray(topNode)
+      ? topNode
+      : `nodes` in topNode
+      ? topNode.nodes!
+      : [topNode],
+    up() {
+      context.parents.pop();
+      context.indexInSelector = prevIndex.shift()!;
+      const currentParents = context.parents;
+      const currentParent = currentParents[currentParents.length - 1];
+      context.nodesInSelector = currentParent
+        ? (currentParent as any).nodes
+        : topNode;
+    },
+    next() {
+      context.indexInSelector++;
+    },
+    insertNested(node) {
+      context.parents = [...context.parents, node];
+      prevParents.push(context.parents);
+      prevIndex.unshift(context.indexInSelector);
+      context.indexInSelector = 0;
+      context.nodesInSelector = node.nodes;
+    },
+  };
+  return context;
 }
 
 walk.skipNested = 0 as const;
 walk.skipCurrentSelector = 1 as const;
 walk.stopAll = Infinity;
 
-function isWithNodes(node: any): node is { nodes: SelectorNode[] } {
+type ContainerWithNodes = Containers & { nodes: SelectorNode[] };
+function isWithNodes(node: any): node is ContainerWithNodes {
   return node && `nodes` in node;
 }
--- packages/css-selector-parser/test/ast-utils.spec.ts
+++ packages/css-selector-parser/test/ast-utils.spec.ts
@@ -16,9 +16,16 @@ function testWalk(
     expectedMap,
   }: {
     walkOptions?: WalkOptions;
-    mapVisit: (node: SelectorNode, parents: SelectorNode[]) => any;
+    mapVisit: (
+      node: SelectorNode,
+      index: number,
+      nodes: SelectorNode[],
+      parents: SelectorNode[]
+    ) => any;
     resultVisit?: (
       node: SelectorNode,
+      index: number,
+      nodes: SelectorNode[],
       parents: SelectorNode[]
     ) => number | undefined;
     expectedMap: any[];
@@ -28,9 +35,18 @@ function testWalk(
   try {
     walk(
       topNode,
-      (current: SelectorNode, parents: SelectorNode[]) => {
-        actual.push(mapVisit ? mapVisit(current, parents) : current);
-        return resultVisit ? resultVisit(current, parents) : undefined;
+      (
+        current: SelectorNode,
+        index: number,
+        nodes: SelectorNode[],
+        parents: SelectorNode[]
+      ) => {
+        actual.push(
+          mapVisit ? mapVisit(current, index, nodes, parents) : current
+        );
+        return resultVisit
+          ? resultVisit(current, index, nodes, parents)
+          : undefined;
       },
       walkOptions
     );
@@ -213,48 +229,215 @@ describe(`ast-utils`, () => {
         ],
       });
     });
-    it(`should provide parent list to visit`, () => {
-      testWalk(parseCssSelector(`:a(:b(:c))`), {
-        mapVisit: ({ type, value }: any, parents: any[]) => ({
-          type,
-          value,
-          parents: parents.map(({ value, type }) => value || type).join(`,`),
-        }),
-        expectedMap: [
-          { type: `selector`, value: undefined, parents: `` },
-          { type: `pseudo_class`, value: `a`, parents: `selector` },
-          { type: `selector`, value: undefined, parents: `selector,a` },
-          { type: `pseudo_class`, value: `b`, parents: `selector,a,selector` },
-          {
-            type: `selector`,
-            value: undefined,
-            parents: `selector,a,selector,b`,
-          },
-          {
-            type: `pseudo_class`,
-            value: `c`,
-            parents: `selector,a,selector,b,selector`,
-          },
-        ],
+    describe(`context`, () => {
+      it(`should provide index and sibling nodes within selector`, () => {
+        testWalk(parseCssSelector(`:a(.a1.a2, .aX.aY), :b(.b1.b2)`), {
+          mapVisit: ({ type, value }: any, index: number, nodes: any[]) => ({
+            type,
+            value,
+            index,
+            nodes: nodes.map(({ value, type }) => value || type).join(`,`),
+          }),
+          expectedMap: [
+            {
+              type: `selector`,
+              value: undefined,
+              index: 0,
+              nodes: `selector,selector`,
+            },
+            { type: `pseudo_class`, value: `a`, index: 0, nodes: `a` },
+            {
+              type: `selector`,
+              value: undefined,
+              index: 0,
+              nodes: `selector,selector`,
+            },
+            {
+              type: `class`,
+              value: `a1`,
+              index: 0,
+              nodes: `a1,a2`,
+            },
+            {
+              type: `class`,
+              value: `a2`,
+              index: 1,
+              nodes: `a1,a2`,
+            },
+            {
+              type: `selector`,
+              value: undefined,
+              index: 1,
+              nodes: `selector,selector`,
+            },
+            {
+              type: `class`,
+              value: `aX`,
+              index: 0,
+              nodes: `aX,aY`,
+            },
+            {
+              type: `class`,
+              value: `aY`,
+              index: 1,
+              nodes: `aX,aY`,
+            },
+            {
+              type: `selector`,
+              value: undefined,
+              index: 1,
+              nodes: `selector,selector`,
+            },
+            { type: `pseudo_class`, value: `b`, index: 0, nodes: `b` },
+            { type: `selector`, value: undefined, index: 0, nodes: `selector` },
+            {
+              type: `class`,
+              value: `b1`,
+              index: 0,
+              nodes: `b1,b2`,
+            },
+            {
+              type: `class`,
+              value: `b2`,
+              index: 1,
+              nodes: `b1,b2`,
+            },
+          ],
+        });
       });
-    });
-    it(`should provide parent list to visit (test multi selectors)`, () => {
-      testWalk(parseCssSelector(`:a(:a1(), :a2), :b(:b1)`), {
-        walkOptions: {
-          ignoreList: [`selector`],
-        },
-        mapVisit: ({ type, value }: any, parents: any[]) => ({
-          type,
-          value,
-          parents: parents.map(({ value, type }) => value || type).join(`,`),
-        }),
-        expectedMap: [
-          { type: `pseudo_class`, value: `a`, parents: `selector` },
-          { type: `pseudo_class`, value: `a1`, parents: `selector,a,selector` },
-          { type: `pseudo_class`, value: `a2`, parents: `selector,a,selector` },
-          { type: `pseudo_class`, value: `b`, parents: `selector` },
-          { type: `pseudo_class`, value: `b1`, parents: `selector,b,selector` },
-        ],
+      it(`should provide index and sibling nodes within selector (skip nodes)`, () => {
+        testWalk(parseCssSelector(`:a(.a1.a2, .aX.aY), :b(.b1.b2)`), {
+          mapVisit: ({ type, value }: any, index: number, nodes: any[]) => ({
+            type,
+            value,
+            index,
+            nodes: nodes.map(({ value, type }) => value || type).join(`,`),
+          }),
+          resultVisit: ({ value }: any) => {
+            if (value === `a1`) {
+              return walk.skipCurrentSelector;
+            } else if (value === `b`) {
+              return walk.skipNested;
+            }
+            return;
+          },
+          expectedMap: [
+            {
+              type: `selector`,
+              value: undefined,
+              index: 0,
+              nodes: `selector,selector`,
+            },
+            { type: `pseudo_class`, value: `a`, index: 0, nodes: `a` },
+            {
+              type: `selector`,
+              value: undefined,
+              index: 0,
+              nodes: `selector,selector`,
+            },
+            {
+              type: `class`,
+              value: `a1`,
+              index: 0,
+              nodes: `a1,a2`,
+            },
+            {
+              type: `selector`,
+              value: undefined,
+              index: 1,
+              nodes: `selector,selector`,
+            },
+            {
+              type: `class`,
+              value: `aX`,
+              index: 0,
+              nodes: `aX,aY`,
+            },
+            {
+              type: `class`,
+              value: `aY`,
+              index: 1,
+              nodes: `aX,aY`,
+            },
+            {
+              type: `selector`,
+              value: undefined,
+              index: 1,
+              nodes: `selector,selector`,
+            },
+            { type: `pseudo_class`, value: `b`, index: 0, nodes: `b` },
+          ],
+        });
+      });
+      it(`should provide parent list to visit`, () => {
+        testWalk(parseCssSelector(`:a(:b(:c))`), {
+          mapVisit: (
+            { type, value }: any,
+            _index: number,
+            _nodes: any[],
+            parents: any[]
+          ) => ({
+            type,
+            value,
+            parents: parents.map(({ value, type }) => value || type).join(`,`),
+          }),
+          expectedMap: [
+            { type: `selector`, value: undefined, parents: `` },
+            { type: `pseudo_class`, value: `a`, parents: `selector` },
+            { type: `selector`, value: undefined, parents: `selector,a` },
+            {
+              type: `pseudo_class`,
+              value: `b`,
+              parents: `selector,a,selector`,
+            },
+            {
+              type: `selector`,
+              value: undefined,
+              parents: `selector,a,selector,b`,
+            },
+            {
+              type: `pseudo_class`,
+              value: `c`,
+              parents: `selector,a,selector,b,selector`,
+            },
+          ],
+        });
+      });
+      it(`should provide parent list to visit (test multi selectors)`, () => {
+        testWalk(parseCssSelector(`:a(:a1(), :a2), :b(:b1)`), {
+          walkOptions: {
+            ignoreList: [`selector`],
+          },
+          mapVisit: (
+            { type, value }: any,
+            _index: number,
+            _nodes: any[],
+            parents: any[]
+          ) => ({
+            type,
+            value,
+            parents: parents.map(({ value, type }) => value || type).join(`,`),
+          }),
+          expectedMap: [
+            { type: `pseudo_class`, value: `a`, parents: `selector` },
+            {
+              type: `pseudo_class`,
+              value: `a1`,
+              parents: `selector,a,selector`,
+            },
+            {
+              type: `pseudo_class`,
+              value: `a2`,
+              parents: `selector,a,selector`,
+            },
+            { type: `pseudo_class`, value: `b`, parents: `selector` },
+            {
+              type: `pseudo_class`,
+              value: `b1`,
+              parents: `selector,b,selector`,
+            },
+          ],
+        });
       });
     });
   });
