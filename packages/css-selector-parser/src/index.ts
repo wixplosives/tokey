b2079a3d597ce3d154fe0579a29b92ee14778c84
Ido Rosenthal <idoros@gmail.com>
2021-06-28 13:53:10 +0300

    chore: merge type import export from index

--- packages/css-selector-parser/src/index.ts
+++ packages/css-selector-parser/src/index.ts
@@ -2,5 +2,4 @@ export { parseCssSelector } from "./selector-parser";
 export * from "./ast-types";
 export { stringifySelectorAst } from "./stringify";
 export { walk } from "./ast-utils";
-import type { WalkOptions } from "./ast-utils";
-export type { WalkOptions };
+export type { WalkOptions } from "./ast-utils";
