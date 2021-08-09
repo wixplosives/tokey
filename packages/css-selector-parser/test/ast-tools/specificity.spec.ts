21aa67081cd24865dcd09cdcca51b66959d011a6
Ido Rosenthal <idoros@gmail.com>
2021-08-09 09:45:23 +0300

    chore: group tests

--- packages/css-selector-parser/test/ast-tools/specificity.spec.ts
+++ packages/css-selector-parser/test/ast-tools/specificity.spec.ts
@@ -51,64 +51,68 @@ describe(`ast-tools/specificity`, () => {
       });
     });
   });
-  it(`should not add specificity for combinators`, () => {
-    const specificity = calcSpecificity(
-      parseCssSelector(`.a + .b ~ .c > .d .e`)
-    );
-    expect(specificity).to.eql([0, 0, 5, 0]);
+  describe(`special cases`, () => {
+    it(`should only add the most specific inner selector for :not()`, () => {
+      const specificity = calcSpecificity(parseCssSelector(`:not(.a, #b)`));
+      expect(specificity).to.eql([0, 1, 0, 0]);
+    });
+    it(`should only add the most specific inner selector for :is()`, () => {
+      const specificity = calcSpecificity(parseCssSelector(`:is(.a, #b)`));
+      expect(specificity).to.eql([0, 1, 0, 0]);
+    });
+    it(`should only add the most specific inner selector for :has()`, () => {
+      const specificity = calcSpecificity(parseCssSelector(`:has(.a, #b)`));
+      expect(specificity).to.eql([0, 1, 0, 0]);
+    });
+    it(`should add zero specificity for :where()`, () => {
+      const specificity = calcSpecificity(parseCssSelector(`:where(.a, #b)`));
+      expect(specificity).to.eql([0, 0, 0, 0]);
+    });
+    it(`should add :nth-child pseudo-class plus the most specific inner selectors`, () => {
+      const specificity = calcSpecificity(
+        parseCssSelector(`:nth-child(5n - 4 of .a, #b)`)
+      );
+      expect(specificity).to.eql([0, 1, 1, 0]);
+    });
+    it(`should add :nth-last-child pseudo-class plus the most specific inner selectors`, () => {
+      const specificity = calcSpecificity(
+        parseCssSelector(`:nth-last-child(5n -4 of .a, #b)`)
+      );
+      expect(specificity).to.eql([0, 1, 1, 0]);
+    });
+    it(`should add :nth-of-type pseudo-class plus the most specific inner selectors`, () => {
+      const specificity = calcSpecificity(
+        parseCssSelector(`:nth-of-type(5n -4 of .a, #b)`)
+      );
+      expect(specificity).to.eql([0, 1, 1, 0]);
+    });
+    it(`should add :nth-last-of-type pseudo-class plus the most specific inner selectors`, () => {
+      const specificity = calcSpecificity(
+        parseCssSelector(`:nth-last-of-type(5n -4 of .a, #b)`)
+      );
+      expect(specificity).to.eql([0, 1, 1, 0]);
+    });
   });
-  it(`should only add the most specific inner selector for :not()`, () => {
-    const specificity = calcSpecificity(parseCssSelector(`:not(.a, #b)`));
-    expect(specificity).to.eql([0, 1, 0, 0]);
-  });
-  it(`should only add the most specific inner selector for :is()`, () => {
-    const specificity = calcSpecificity(parseCssSelector(`:is(.a, #b)`));
-    expect(specificity).to.eql([0, 1, 0, 0]);
-  });
-  it(`should only add the most specific inner selector for :has()`, () => {
-    const specificity = calcSpecificity(parseCssSelector(`:has(.a, #b)`));
-    expect(specificity).to.eql([0, 1, 0, 0]);
-  });
-  it(`should add zero specificity for :where()`, () => {
-    const specificity = calcSpecificity(parseCssSelector(`:where(.a, #b)`));
-    expect(specificity).to.eql([0, 0, 0, 0]);
-  });
-  it(`should add :nth-child pseudo-class plus the most specific inner selectors`, () => {
-    const specificity = calcSpecificity(
-      parseCssSelector(`:nth-child(5n - 4 of .a, #b)`)
-    );
-    expect(specificity).to.eql([0, 1, 1, 0]);
-  });
-  it(`should add :nth-last-child pseudo-class plus the most specific inner selectors`, () => {
-    const specificity = calcSpecificity(
-      parseCssSelector(`:nth-last-child(5n -4 of .a, #b)`)
-    );
-    expect(specificity).to.eql([0, 1, 1, 0]);
-  });
-  it(`should add :nth-of-type pseudo-class plus the most specific inner selectors`, () => {
-    const specificity = calcSpecificity(
-      parseCssSelector(`:nth-of-type(5n -4 of .a, #b)`)
-    );
-    expect(specificity).to.eql([0, 1, 1, 0]);
-  });
-  it(`should add :nth-last-of-type pseudo-class plus the most specific inner selectors`, () => {
-    const specificity = calcSpecificity(
-      parseCssSelector(`:nth-last-of-type(5n -4 of .a, #b)`)
-    );
-    expect(specificity).to.eql([0, 1, 1, 0]);
-  });
-  it(`should handle nested pseudo classes`, () => {
-    const specificity = calcSpecificity(
-      parseCssSelector(`:is(:where(#zero), :has(:not(span, #a), .a))`)
-    );
-    expect(specificity).to.eql([0, 1, 0, 0]);
-  });
-  it(`should not take non native functional selectors arguments into account`, () => {
-    const specificity = calcSpecificity(
-      parseCssSelector(
-        `div(#a).x(#b)::y(#c)[attr](#d)&(#e)*(#f)#id(#g):unknown(#h)`
-      )
-    );
-    expect(specificity).to.eql([0, 1, 3, 2]);
+  describe(`complex selectors`, () => {
+    it(`should not add specificity for combinators`, () => {
+      const specificity = calcSpecificity(
+        parseCssSelector(`.a + .b ~ .c > .d .e`)
+      );
+      expect(specificity).to.eql([0, 0, 5, 0]);
+    });
+    it(`should handle nested pseudo classes`, () => {
+      const specificity = calcSpecificity(
+        parseCssSelector(`:is(:where(#zero), :has(:not(span, #a), .a))`)
+      );
+      expect(specificity).to.eql([0, 1, 0, 0]);
+    });
+    it(`should not take non native functional selectors arguments into account`, () => {
+      const specificity = calcSpecificity(
+        parseCssSelector(
+          `div(#a).x(#b)::y(#c)[attr](#d)&(#e)*(#f)#id(#g):unknown(#h)`
+        )
+      );
+      expect(specificity).to.eql([0, 1, 3, 2]);
+    });
   });
 });
